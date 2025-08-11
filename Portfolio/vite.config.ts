7159b305cb97efb200e4b86ac0af2c500b157352
VladRubert <73608630+VladRubert@users.noreply.github.com>
2025-08-10 18:00:28 -0600

    Update vite.config.ts

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -1,14 +1,7 @@
-import path from "path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Para que funcione correctamente con GitHub Pages
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/DS/', // 👈 importante para GitHub Pages
 })
